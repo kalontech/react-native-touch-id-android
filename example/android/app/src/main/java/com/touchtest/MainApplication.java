9732e94ec554ad5a6d407edbab65f594e073ce48
Vasiliev Mikhail <vasiliev.m@ukr.net>
2017-03-22 13:10:46 +0200

    travis CI integration

---
+++ example/android/app/src/main/java/com/touchtest/MainApplication.java
@@ -0,0 +1,45 @@
+package com.touchtest;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+import com.github.ajalt.reprint.core.Reprint;
+
+import java.util.Arrays;
+import java.util.List;
+
+import co.eleken.react_native_touch_id_android.FingerprintPackage;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new FingerprintPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+    Reprint.initialize(this);
+  }
+}
