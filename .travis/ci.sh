5c73cd764a955e267591aafec84d1d26f9356b03
Vasiliev Mikhail <vasiliev.m@ukr.net>
2017-03-22 13:32:24 +0200

    changed travis commands

--- .travis/ci.sh
+++ .travis/ci.sh
@@ -4,11 +4,11 @@ case "${TRAVIS_OS_NAME}" in
   osx)
     cd example_tmp
     set -o pipefail && npm run build:ios | xcpretty -c -f `xcpretty-travis-formatter`
-    npm run test:ios
+    npm run test
   ;;
   linux)
     cd example_tmp
-    npm run build:android
-    npm run test:android
+    npm run android:build
+    npm run test
   ;;
 esac
